bcaf641a2d466edd3c44c9585bfba498356ec247
yuwu <wuyu621@outlook.com>
2024-10-31 13:18:13 -0700

    fix supabase bug for building

--- utils/superbase.ts
+++ utils/superbase.ts
@@ -3,8 +3,8 @@ import { createClient } from "@supabase/supabase-js";
 const bucket = "main-bucket";
 
 export const supabase = createClient(
-  process.env.SUPABASE_URL as string,
-  process.env.SUPABASE_KEY as string
+  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
+  process.env.NEXT_PUBLIC_SUPABASE_KEY as string
 );
 
 export const uploadImage = async (image: File) => {
