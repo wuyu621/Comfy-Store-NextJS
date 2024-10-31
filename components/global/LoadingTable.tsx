386e785103eab7203badad01a9da23022977f750
yuwu <wuyu621@outlook.com>
2024-10-31 12:56:50 -0700

    fix two bugs for build

--- components/global/LoadingTable.tsx
+++ components/global/LoadingTable.tsx
@@ -1,6 +1,6 @@
 import { Skeleton } from "../ui/skeleton";
 
-function LoadingTable({ rows = 6 }: { rows: number }) {
+function LoadingTable({ rows = 6 }: { rows?: number }) {
   const tableRows = Array.from({ length: rows }, (_, index) => {
     return (
       <div className="mb-4" key={index}>
